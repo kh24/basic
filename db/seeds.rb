a8b40e467f59af9de2a51f954fc19d57e5303977
Kazumi <kazumi.ah@gmail.com>
2014-06-22 16:23:59 -0400

    Associated users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
 require 'faker'
 
+5.times do
+  user = User.new(
+    name:  Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker:: Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+users = User.all
+
 #create posts
 50.times do 
   Post.create(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
  )
@@ -18,5 +30,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
