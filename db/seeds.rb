3c6f73f40841a2f0f6d7960d087a2c981cab6afd
Kazumi <kazumi.ah@gmail.com>
2014-06-22 18:53:25 -0400

    Associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'faker'
 
+#Create Users
 5.times do
   user = User.new(
     name:  Faker::Name.name,
@@ -11,10 +12,49 @@ require 'faker'
 end
 users = User.all
 
+#Create an admin
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+  )
+admin.skip_confirmation!
+admin.save
+
+#Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
+  )
+moderator.skip_confirmation!
+moderator.save
+
+#Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld',
+  )
+member.skip_confirmation!
+member.save
+
+# Create Topics
+15.times do
+  Topic.create(
+    name:  Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
 #create posts
 50.times do 
   Post.create(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
  )
